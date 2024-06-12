dce5cae7776e3a5670b65f5fbb607a3d36ba68cf
k-wakamatsu-tms <69659176+k-wakamatsu-tms@users.noreply.github.com>
2024-06-12 19:24:11 +0900

    firstcommit

---
+++ apps/expo/app/index.tsx
@@ -0,0 +1,15 @@
+import { HomeScreen } from 'app/features/home/screen'
+import { Stack } from 'expo-router'
+
+export default function Screen() {
+  return (
+    <>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          title: 'Home',
+        }}
+      />
+    </>
+  )
+}
